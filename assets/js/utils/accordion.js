8a37259cce55f809d3b61a695dff3eb69f736f41
Ali Shariatian <shariatian92@gmail.com>
2024-08-28 13:59:02 +0330

    git init

---
+++ assets/js/utils/accordion.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionItems = document.querySelectorAll(".accordion .item");
+
+  accordionItems.forEach((item) => {
+    const title = item.querySelector(".accordion .item .title");
+    const text = item.querySelector(".accordion .item .text");
+
+    title.addEventListener("click", () => {
+      const isActive = text.classList.contains("show");
+
+      // Hide all sections
+      accordionItems.forEach((item) => item.querySelector(".text").classList.remove("show"));
+
+      // Toggle the clicked section
+      if (!isActive) {
+        text.classList.add("show");
+      }
+    });
+  });
+});
---
+++ assets/js/utils/tabs.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".portfolio .tabs span");
+  const items = document.querySelectorAll(".portfolio .items .item");
+  let activeCategory = null; // Keep track of the currently active category
+
+  // Initially display all items
+  updateItemsDisplay(null);
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", function () {
+      // If the same tab is clicked again, show all items
+      if (activeCategory === tab.dataset.category) {
+        activeCategory = null; // Reset active category
+        tabs.forEach((t) => t.classList.remove("active")); // Remove 'active' class from all tabs
+        updateItemsDisplay(null); // Show all items
+      } else {
+        // Set the new active category
+        activeCategory = tab.dataset.category;
+
+        // Remove 'active' class from all tabs
+        tabs.forEach((t) => t.classList.remove("active"));
+
+        // Add 'active' class to the clicked tab
+        tab.classList.add("active");
+
+        // Update the displayed items based on the clicked tab
+        updateItemsDisplay(activeCategory);
+      }
+    });
+  });
+
+  function updateItemsDisplay(category) {
+    items.forEach((item) => {
+      if (!category || item.dataset.category === category) {
+        item.style.display = "block"; // Show item
+      } else {
+        item.style.display = "none"; // Hide item
+      }
+    });
+  }
+});
